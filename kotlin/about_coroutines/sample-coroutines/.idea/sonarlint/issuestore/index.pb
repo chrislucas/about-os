d2e262d1803e94a60f5ee631da366fecefd0afe8
chrislucas <christoffer.luccas@gmail.com>
2019-11-18 07:18:34 -0300

    estudo sobre coroutines, LWP e conceitos de S.O

---
+++ kotlin/about_coroutines/sample-coroutines/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+?
+settings.gradle,0\5\05efc8b1657769a27696d478ded1e95f38737233
+G
+raw/lightweight-threads,e\6\e65a97badacbcf39f4d4780d8031b0f367a3f228
+<
+build.gradle,f\0\f07866736216be0ee2aba49e392191aeae700a35
+I
+src/main/kotlin/Basics.kt,7\5\7595f1f30d73ff04a518f257c311674e961c6d22
+>
+raw/android-os,6\c\6cc770fa40f2ce2f7993277dfd3400b1da4c3e04
+=
+raw/os-search,c\0\c051b85cdf10bc56332ee71e2d651f6a14b72d11
---
+++ kotlin/about_coroutines/sample-coroutines/src/main/kotlin/Basics.kt
@@ -0,0 +1,47 @@
+
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+fun main() {
+
+    /**
+     * Dispara uma nova coroutine sem bloquear a thread atual onde
+     * ela foi disparada e retorna uma referencia para coroutine
+     * como um Job
+     * {@link Job}
+     *
+     * @param context: CoroutineContext = EmptyCoroutineContext
+     *
+     *
+     * @param start: CoroutinesStart = CoroutineStart.DEFAULT
+     *
+     * @param block: suspend CoroutinesScope.() -> Unit
+     * */
+    val job = GlobalScope.launch {
+        // funcao com o comportamento similar a Thread.sleep(ms)
+        // com a vantagem de nao bloquear a thread mas suspender a corotina
+        delay(200L)
+        println("Inside Launch: ${0xff}")
+    }
+    println("Outside: ${0xcc}")
+    // THread sleep bloqueia a Thread principal por 500ms e manter a JVM ativa
+    Thread.sleep(500L)
+
+    job.let {
+        it.children.let {
+            seq ->
+            if (seq.count() > 0) {
+                println("Loggin Job children")
+                seq.iterator().forEach { job -> printJobData(job) }
+            }
+        }
+        println("Logging job itself")
+        printJobData(it)
+    }
+}
+
+fun printJobData(job: Job) {
+    println("${job.isActive}, ${job.key}, ${job.isCompleted}")
+}
