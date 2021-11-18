58f5a05243ae47f351dd46c9948f326ff0e95efb
christoffer lucas <christoffer.santos@mercadolivre.com>
2021-11-18 03:43:55 -0300

    links para estudos

---
+++ kotlin/about_coroutines/sample-coroutines/src/main/kotlin/sample/ktutorial/functional/package-info.java
@@ -0,0 +1,7 @@
+package sample.ktutorial.functional;
+
+/*
+    https://kt.academy/article/ek-function-types
+    https://blog.kotlin-academy.com/kotlin-programmer-dictionary-function-type-vs-function-literal-vs-lambda-expression-vs-anonymous-edc97e8873e
+    https://kotlinlang.org/docs/fun-interfaces.html#functional-interfaces-vs-type-aliases
+ */
